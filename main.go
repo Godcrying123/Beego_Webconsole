bf15306ba5c07dea216fc8ba26f02078d398d083
Godcrying123 <jackson.zhang1@hotmail.com>
2019-08-05 22:45:01 +0800

    finish the linux command execution

--- main.go
+++ main.go
@@ -2,14 +2,10 @@ package main
 
 import (
 	_ "webconsole_sma/routers"
-	"webconsole_sma/utils"
 
 	"github.com/astaxie/beego"
 )
 
 func main() {
-	//beego.Run()
-	strout, err := utils.Command_Exec("df -ah")
-	beego.Info(strout)
-	beego.Error(err)
+	beego.Run()
 }
--- utils/commad_utils.go
+++ utils/commad_utils.go
@@ -10,33 +10,31 @@ import (
 
 func init() {}
 
-func Command_Exec(command string) (result string, err error) {
+func CommandExec(command string) (result string, err error) {
 	var builder strings.Builder
-	command_exec := exec.Command("/bin/bash", "-c", command)
-	stdout, err := command_exec.StdoutPipe()
+	commandexec := exec.Command("/bin/bash", "-c", command)
+	stdout, err := commandexec.StdoutPipe()
 	if err != nil {
 		beego.Error(err)
 		return "", err
 	}
 
-	if err := command_exec.Start(); err != nil {
+	if err := commandexec.Start(); err != nil {
 		beego.Error(err)
 		return "", err
 	}
-	beego.Info(stdout)
 	outputBuf := bufio.NewReader(stdout)
-	beego.Info(outputBuf)
 	for {
 		output, _, err := outputBuf.ReadLine()
 		if err != nil {
 			if err.Error() != "EOF" {
 				beego.Error(err)
+				return "", err
+			} else {
+				return builder.String(), nil
 			}
-			return "", err
 		}
 		builder.Write(output)
 		builder.Write([]byte("\n"))
 	}
-
-	return builder.String(), nil
 }
