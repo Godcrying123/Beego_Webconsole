5009242ec0a3b04e7bc5577f681800d6882309f4
root <root@SHCITSMACORECPE21.hpeswlab.net>
2019-08-22 15:19:36 +0800

    finish the function of webshell

--- controllers/websocketController.go
+++ controllers/websocketController.go
@@ -1,7 +1,6 @@
 package controllers
 
 import (
-	"bytes"
 	"net/http"
 	"time"
 	"webconsole_sma/utils"
@@ -13,6 +12,9 @@ import (
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
 }
 
 type HostWebSocketController struct {
@@ -33,9 +35,6 @@ type SSHWebSocketController struct {
 
 func (this *HostWebSocketController) Get() {
 	tick := time.Tick(4 * time.Second)
-	upgrader.CheckOrigin = func(r *http.Request) bool {
-		return true
-	}
 	ws, err := upgrader.Upgrade(this.Ctx.ResponseWriter, this.Ctx.Request, nil)
 	if err != nil {
 		beego.Error(err)
@@ -56,9 +55,6 @@ func (this *HostWebSocketController) Get() {
 
 func (this *ServiceWebSocketController) Get() {
 	tick := time.Tick(20 * time.Second)
-	upgrader.CheckOrigin = func(r *http.Request) bool {
-		return true
-	}
 	ws, err := upgrader.Upgrade(this.Ctx.ResponseWriter, this.Ctx.Request, nil)
 	if err != nil {
 		beego.Error(err)
@@ -80,33 +76,28 @@ func (this *ServiceWebSocketController) Get() {
 }
 
 func (this *SSHWebSocketController) Get() {
-	upgrader.CheckOrigin = func(r *http.Request) bool {
-		return true
+	sshClient, err := utils.NewSshClient()
+	if err != nil {
+		beego.Error(err)
 	}
+	defer sshClient.Close()
+	// startTime := time.Now()
+	sshConn, err := utils.NewSshConn(120, 32, sshClient)
+	if err != nil {
+		beego.Error(err)
+	}
+	defer sshConn.Close()
 	ws, err := upgrader.Upgrade(this.Ctx.ResponseWriter, this.Ctx.Request, nil)
 	if err != nil {
 		beego.Error(err)
 	}
 	defer ws.Close()
 	utils.SSHClients[ws] = true
-	go handleMessages()
-	var byteBuilder bytes.Buffer
-	for {
-		for client := range utils.SSHClients {
-			_, input, err := client.ReadMessage()
-			if err != nil {
-				beego.Error(err)
-				client.Close()
-				delete(utils.SSHClients, client)
-			}
-			if bytes.Equal(input, []byte{13}) {
-				beego.Info(byteBuilder.String())
-				byteBuilder.Reset()
-			} else {
-				byteBuilder.Write(input)
-			}
-		}
-	}
+	quitChan := make(chan bool, 3)
+	go sshConn.ReceiveWsMsg(ws, quitChan)
+	go sshConn.SendComboOutput(ws, quitChan)
+	go sshConn.SessionWait(quitChan)
+	<-quitChan
 }
 
 func handleMessages() {
